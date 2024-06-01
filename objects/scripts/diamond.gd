058ddab7822dbaa27627b2190f181a355dc40785
juvenal <juvenalsantiso@gmail.com>
2024-06-01 17:53:53 +0200

    Picking diamond

--- objects/scripts/diamond.gd
+++ objects/scripts/diamond.gd
@@ -1,11 +1,5 @@
 extends Area2D
 
-
-
-
-func _on_area_entered(area):
-	print("hola diamond")
-
-
 func _on_body_entered(body):
-	print("hola diamond")
+	if body.has_method("picked_diamond"): #TODO: do picked diamond
+		body.picked_diamond()
